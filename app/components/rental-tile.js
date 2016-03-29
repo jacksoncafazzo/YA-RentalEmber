ac3e5b2f2a16a0690992881f4f58f49e227eff18
Michelle Brecunier and tyler suderman <tcsuder@gmail.com>
2016-03-28 17:03:07 -0700

    mid-build

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -6,7 +6,7 @@ export default Ember.Component.extend({
     imageShow: function() {
       this.set('isImageShowing', true);
     },
-    imageHide: function() {
+    imageHide() {
       this.set('isImageShowing', false);
     },
     delete(rental) {
